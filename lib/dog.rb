e98bf5bc57f008a2783bb54a720669b0d5c34547
William Yu <wy427@nyu.edu>
2022-03-17 19:07:59 -0400

    self.create_table added

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
+  attr_accessor :name, :id, :breed
 
+  def initialize(name:, breed:, id: nil)
+    @breed = breed
+    @name = name
+    @id = id
+    end
+
+    #create_table
+
+    def self.create_table
+        sql = <<- SQL
+            CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT, 
+                breeed TEXT
+            )
+        SQL
+        DB[:conn].execute(sql)
+    end
 end
