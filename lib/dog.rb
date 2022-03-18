f0d674330441152215e5818bdb16d5747326fc3a
William Yu <wy427@nyu.edu>
2022-03-17 23:31:50 -0400

    done except bonus methods

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,75 +7,80 @@ class Dog
         @breed = breed
     end
 
-    #create_table - create the table
+    #create_table
 
-    def self.create_table
+    def self.create_table(name:,breed:)
         sql = <<- SQL
-            CREATE TABLE IF NOT EXISTS dogs (
-                id INTEGER PRIMARY KEY,
-                name TEXT, 
-                breed TEXT
-            )
+        CREATE TABLE IF NOT EXISTS dogs (
+             id INTEGER PRIMARY KEY,
+             name TEXT, 
+             breed TEXT,
+        )
         SQL
         DB[:conn].execute(sql)
     end
 
-    #drop_table --> drop the dogs table from database
+    #drop_table
+
     def drop_table
-        sql = "DROP TABLE IF EXISTS dogs"
+        sql = "DROP TABLE IF NOT EXISTS dogs"
         DB[:conn].execute(sql)
-        # DB[:conn].execute("DROP TABLE IF EXISTS dogs")
     end
 
-    #save - add row to sql database
-
-    def save 
+    #save - insert new row
+    def save
         sql = <<- SQL
-        INSERT INTO dogs (name,breed)
-        VALUES (?,?)
+            INSERT INTO dogs (name,breed)
+            VALUES (?,?)
         SQL
-
-        #INSERT THE SONG
-        DB[:conn].execute(sql,self.name,self.breed)
-
-        #get the song id from the database
-        self.id = DB[:conn].execute("SELECT last_insert_row FROM dogs")[0][0]
-
-        # return RUBY instance
+        DB[:conn].execute(sql, self.name, self.breed)
+        #update current id
+        self.id = DB[:conn].execute("SELECT last_inserted_row() FROM dogs")[0][0]
+        
         self
     end
 
-
-    #create new instance and save it
-    def self.create(name:, album:)
-        song = Song.new(name: name, album: album)
+   #create
+    def self.create(name:,breed:)
+        dog = Dog.new(name:name,breed:breed)
+        dog.save
     end
 
-
-    #.new_from_db
-
-    def self.new_from_db
-        self.new(id: row[0], name:row[1], breed: row[2])
+    #new_from_db
+    def new_from_db
+        self.new(id: row[0], name: row[1], breed: row[2])
     end
-
-    #all
 
     def self.all
-        sql = <<-SQL
+        sql = <<- SQL
             SELECT * 
             FROM dogs;
         SQL
-        # select all fror each row
-        DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
+
+        DB[:conn].execute(sql).map{|row| self.new_from_db(row)}
+    end
+
+    def self.find_by_name(name)
+        sql = <<- SQL
+            SELECT * 
+            FROM dogs
+            WHERE dogs.name = ?
+            LIMIT 1;
+        SQL
+
+        DB[:conn].execute(sql,self.name).map{|row| self.new_from_db(row)}.first 
     end
 
 
-
-
-
-
-
-
+    def self.find(id)
+        sql = <<- SQL
+            SELECT * 
+            FROM dogs
+            WHERE dogs.id = ?
+            LIMIT 1;
+        SQL
+        DB[:conn].execute(sql,self.id).map{|row| self.new_from_db(row)}.first
+    end
 end
 
 
