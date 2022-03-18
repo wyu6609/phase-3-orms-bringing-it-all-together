64b496be0a7a02502892c5b5e0773156ddb23697
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-03-18 10:43:54 -0400

    completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,110 +1,85 @@
 class Dog
   attr_accessor :name, :breed, :id
 
-    def initialize(name:, breed:, id: nil)
-        @id = id
-        @name = name
-        @breed = breed
-    end
+  def initialize(name:, breed:, id: nil)
+    @id = id
+    @name = name
+    @breed = breed
+  end
 
-    #create_table
+  #create table
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs(
+          id INTEGER PRIMARY KEY,
+          name TEXT, 
+          breed TEXT
+      )
+    SQL
 
-    def self.create_table(name:,breed:)
-        sql = <<- SQL
-        CREATE TABLE IF NOT EXISTS dogs (
-             id INTEGER PRIMARY KEY,
-             name TEXT, 
-             breed TEXT,
-        )
-        SQL
-        DB[:conn].execute(sql)
-    end
+    DB[:conn].execute(sql)
+  end
 
-    #drop_table
+  # drop dogs table from DB
 
-    def drop_table
-        sql = "DROP TABLE IF NOT EXISTS dogs"
-        DB[:conn].execute(sql)
-    end
+  def self.drop_table
+    sql = 'DROP TABLE IF EXISTS dogs'
+    DB[:conn].execute(sql)
+  end
 
-    #save - insert new row
-    def save
-        sql = <<- SQL
-            INSERT INTO dogs (name,breed)
-            VALUES (?,?)
-        SQL
-        DB[:conn].execute(sql, self.name, self.breed)
-        #update current id
-        self.id = DB[:conn].execute("SELECT last_inserted_row() FROM dogs")[0][0]
-        
-        self
-    end
+  #save -- insert new record into the database & return the instance
+  def save
+    sql = <<-SQL
+    INSERT INTO dogs (name, breed)
+    VALUES(?,?)
+    SQL
 
-   #create
-    def self.create(name:,breed:)
-        dog = Dog.new(name:name,breed:breed)
-        dog.save
-    end
+    DB[:conn].execute(sql, self.name, self.breed)
+    self.id = DB[:conn].execute('SELECT last_insert_rowid() FROM dogs')[0][0]
 
-    #new_from_db
-    def new_from_db
-        self.new(id: row[0], name: row[1], breed: row[2])
-    end
+    self
+  end
 
-    def self.all
-        sql = <<- SQL
-            SELECT * 
-            FROM dogs;
-        SQL
+  def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
+    dog.save
+  end
 
-        DB[:conn].execute(sql).map{|row| self.new_from_db(row)}
-    end
+  #create new row in DB, return new instance of DOG CLASS
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], breed: row[2])
+  end
 
-    def self.find_by_name(name)
-        sql = <<- SQL
-            SELECT * 
+  #all- return an array of Dog instances for every record in the dogs table
+  def self.all
+    sql = <<-SQL
+        SELECT *
+        FROM dogs;
+    SQL
+    DB[:conn].execute(sql).map { |row| self.new_from_db(row) }
+  end
+
+  # insert a dog into the database, find it by calling the find_by_name
+  def self.find_by_name(name)
+    sql = <<-SQL
+            SELECT *
             FROM dogs
-            WHERE dogs.name = ?
+            WHERE dogs.name = ? 
             LIMIT 1;
         SQL
+    DB[:conn].execute(sql, name).map { |row| self.new_from_db(row) }.first
+  end
 
-        DB[:conn].execute(sql,self.name).map{|row| self.new_from_db(row)}.first 
-    end
+  #takes in an ID, should return a single DOG instance
 
-
-    def self.find(id)
-        sql = <<- SQL
-            SELECT * 
-            FROM dogs
-            WHERE dogs.id = ?
-            LIMIT 1;
+  def self.find(id)
+    sql = <<-SQL
+          SELECT *
+          FROM dogs
+          WHERE dogs.id = ?
+          LIMIT 1;
         SQL
-        DB[:conn].execute(sql,self.id).map{|row| self.new_from_db(row)}.first
-    end
-end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    DB[:conn].execute(sql, id).map { |row| self.new_from_db(row) }.first
+  end
 end
